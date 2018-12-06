591cffbecdae499e5c00622ebd1195c1b614f5cb
NodyXiao <nody_xiao@mail.chungyo.net>
2018-12-06 16:50:57 +0800

    edit and add ajax

--- edit_personal.php
+++ edit_personal.php
@@ -1,11 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Message Board</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    
+    <script>
+      function edit() {
+          // var msgid = $('#del_msgid').val();
+          $.ajax({
+              url: 'ajax_edit_personal.php',
+              // data: {'msgid':msgid},
+              type: 'POST',
+              dataType: 'json',
+              success: function(d) {
+                  console.log(d);
+                  // $('#del_check').modal('hide');
+              }
+          });
+      }
 
-require_once(__DIR__.'/config.php');
+    </script>
 
-ini_set("display_errors","On");
-
-session_start();
-
-echo "<pre>";print_r($_SESSION);
-
-?>
+    <body>
+        <br><br><br><br>
+        <div class="container">
+            <h3 class="text-center">修改個人資料</h3>
+            <hr>
+        </div>
+        <div class="container">
+          <div class="col-md-6 col-md-offset-3">
+              <form id="editform" name="editform" method="POST">
+                  <input id="pass1" name="pass1" class="form-control input-lg" type="password" placeholder="輸入姓名(Name)"/>
+                  <p id="p1" name="p1" style="color: red; font-size: 14px; opacity: 0; margin: 0" >GodMartin</p>
+                  <input id="pass2" name="pass2" class="form-control input-lg" type="password" placeholder="輸入暱稱(Nick)"/>
+                  <p id="p2" name="p2" style="color: red; font-size: 14px; opacity: 0; margin: 0" >GodMartin</p>
+                  <button id="doedit" name="doedit" class="btn btn-primary btn-lg btn-block" type="button" onclick="edit();">修改</button>
+              </form>
+              <br>
+              <a class="btn btn-default btn-lg btn-block" href="manager.php">管理者首頁</a>                                                                  
+          </div>
+        </div>
+        <br>
+    </body>
+</html>
